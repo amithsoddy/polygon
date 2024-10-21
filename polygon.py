d156d4cab52212fcc4ec683edfd89e8e57105995
amithsoddy <anithasoddy@gmail.com>
2024-10-21 15:28:22 +0530

    polygon

---
+++ polygon.py
@@ -0,0 +1,12 @@
+class polygon:
+    def __init__(self,sides,length):
+        self.side=sides
+        self.side=5
+        self.length=6
+    def display(self):
+        print(self.side)
+        print(self.length)
+class shape(polygon):
+    def __init__(self,sides,length,area):
+        self.area=area
+        self.area=square
